2ab374aa9ac8ec3aff4f790520bfa6a6ad6ef0f2
Kurligin <68651449+Kurligin@users.noreply.github.com>
2023-04-25 19:59:02 +0300

    v0.1

---
+++ server/registration.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api()
+
+
+class Main(Resource):
+    def get(self):
+        return {"info": "Some info", "num": 56}
+
+
+api.add_resource(Main, "/api/main")
+api.init_app(app)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8080, host="127.0.0.1")
